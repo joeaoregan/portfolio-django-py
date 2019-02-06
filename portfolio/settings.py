2efc968d3667c2b715897a7a95f0ad84affb6ca3
Joe O'Regan <joeaoregan@yahoo.ie>
2019-02-06 11:46:02 +0000

    add postgres server

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'portfoliodb',
+		'USER': 'postgres',
+		'PASSWORD': 'django1234',
+		'HOST': 'localhost',
+		'PORT': '5432',
     }
 }
 
