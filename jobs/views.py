9b4c05003f13aed3977e4ddddad654decac34653
Joe O'Regan <joeaoregan@yahoo.ie>
2019-02-06 13:22:11 +0000

    add job details page

--- jobs/views.py
+++ jobs/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Job
 
 # Create your views here.
@@ -7,5 +7,7 @@ def home(request):
 	return render(request, 'jobs/home.html', {'jobs': jobs})
 	
 def detail(request, job_id):
-	print(job_id)
-	return render(request, 'jobs/home.html')
+	# print(job_id) # show job id in console
+	job_detail = get_object_or_404(Job, pk=job_id)
+	# return render(request, 'jobs/home.html')
+	return render(request, 'jobs/detail.html', {'job':job_detail})
