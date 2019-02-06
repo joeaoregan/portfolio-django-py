e176095f8916893c09cc7020a38adb9f2755648c
Joe O'Regan <joeaoregan@yahoo.ie>
2019-02-06 11:29:34 +0000

    Add home html

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+import jobs.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+	path('home', jobs.views.home, name='home'),
 ]
