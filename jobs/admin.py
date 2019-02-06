d1729b6fd21704e1ecea25ab0b6bb117f9573f5e
Joe O'Regan <joeaoregan@yahoo.ie>
2019-02-06 18:42:21 +0000

    Add Job name field, and change displayed admin fields

--- jobs/admin.py
+++ jobs/admin.py
@@ -2,4 +2,8 @@ from django.contrib import admin
 from .models import Job
 
 # Register your models here.
-admin.site.register(Job)
+# admin.site.register(Job)
+
+@admin.register(Job)
+class JobAdmin(admin.ModelAdmin):
+	list_display = ['name', 'summary', 'link']
