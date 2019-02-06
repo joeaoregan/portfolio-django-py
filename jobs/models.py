eb50d8982e435c26935d54f152c7740c8cbcf3a2
Joe O'Regan <joeaoregan@yahoo.ie>
2019-02-06 18:42:21 +0000

    Add Job name field, and change displayed admin fields

--- jobs/models.py
+++ jobs/models.py
@@ -2,12 +2,14 @@ from django.db import models
 
 # Create your models here.
 class Job(models.Model):
+	name = models.CharField(max_length=50, default="", null=True, blank=True)
 	# Images
 	image = models.ImageField(upload_to='images/')
 	# summary
 	summary = models.CharField(max_length=200)
 	# path (github link etc.)
-	link = models.CharField(max_length=200, default="")	# need default value to add to existing entries in database
+	link = models.CharField(max_length=200, default="", null=True, blank=True)	# need default value to add to existing entries in database
 	
 	def __str__(self):
+		# return self.summary
 		return self.summary
